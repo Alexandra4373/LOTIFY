1f6a1144a5576d9d0c9b7ecba4117d180900ae23
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-06 08:28:29 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import Saly from "./assets/Saly.png";
+import Facebook from "./assets/Facebook.png";
+import Apple from "./assets/apple.png";
+import Google from "./assets/google.png";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#fff] ">
+        <div className="">
+          <h3>Your logo</h3>
+        </div>
+        <div className="">
+          <div className="">
+            <h2>Sign in to</h2>
+            <h4>Lorem Ipsum is simply</h4>
+          </div>
+          <div className="">
+            <h5>Sign in</h5>
+            <div className="">
+              <label>
+                <input
+                  type="text"
+                  placeholder="Enter email or user name"
+                ></input>
+              </label>
+            </div>
+
+            <div className="">
+              <label>
+                <input type="text" placeholder="Password"></input>
+              </label>
+
+              <p>Forgot password?</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <p>
+            If you don't have an account <br />
+            You can <span>Register here!</span>
+          </p>
+        </div>
+
+        <div className="">
+          <img src={Saly} />
+        </div>
+
+        <div className="">
+          <button type="button">Login</button>
+          <div className="">
+            <p>or continue with</p>
+
+            <div className="">
+              <img src={Facebook} />
+              <img src={Apple} />
+              <img src={Google} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
